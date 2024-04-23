f7e415bef878ad721c9d5e9d1ac562487c364a68
Lans12345 <olanalans12345@gmail.com>
2024-04-24 07:51:49 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_waste_web/screens/home_screen.dart';
 import 'package:smart_waste_web/screens/login_screen.dart';
 import 'package:smart_waste_web/screens/tabs/user_records_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyArBjull05oz-c1nlmpS3E_wLcDB_YQR1g",
+          authDomain: "plastic-bottle-classifie-4077d.firebaseapp.com",
+          projectId: "plastic-bottle-classifie-4077d",
+          storageBucket: "plastic-bottle-classifie-4077d.appspot.com",
+          messagingSenderId: "744621655610",
+          appId: "1:744621655610:web:791a4470c8b0958cd81703"));
   runApp(const MyApp());
 }
 
